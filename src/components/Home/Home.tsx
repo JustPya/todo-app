6464f1ef32fb0c96a4a4fc2c395bb7381691881a
Fernanda Moreno <monica.moreno@zemoga.com>
2023-04-03 18:29:54 -0500

    Feat: Add base dummy components

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,8 @@
+import TodoList from '../../components/TodoList/TodoList';
+const HelloWorld = (): JSX.Element => {
+    return (
+      <TodoList />
+    );
+  }
+  
+  export default HelloWorld;
---
+++ src/components/TodoList/TodoList.tsx
@@ -0,0 +1,9 @@
+const TodoList = (): JSX.Element => {
+    return (
+      <div>
+        <p>Welcome to my React TypeScript app.</p>
+      </div>
+    );
+  }
+  
+  export default TodoList;
