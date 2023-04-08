f00fd385977d924a2189f38b946c7bf257c0064c
Fernanda Moreno <monica.moreno@zemoga.com>
2023-04-08 00:30:16 -0500

    Conf: Add Storybook

---
+++ src/components/TodoList/TodoList.stories.ts
@@ -0,0 +1,12 @@
+import type { Meta } from '@storybook/react';
+import TodoList from './TodoList';
+
+// Define the meta data for the component
+const meta: Meta = {
+  title: 'TodoList',
+  component: TodoList
+};
+
+// Export the component and stories
+export { TodoList };
+export default meta;
